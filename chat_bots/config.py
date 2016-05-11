e64fa4b8cf4adfbaf4e9b75a1fc4862b8130f7dc
Tejash <tejash@ubuntu.ubuntu-domain>
2016-05-11 19:41:51 +0530

    config changed final1

--- chat_bots/config.py
+++ chat_bots/config.py
@@ -1,3 +1,3 @@
-access_token = "eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJleHAiOjIzMjY4NjI5OTg5MTF9.FuGVh4xJHlF7Yd6sPyybL55XdNN3aYBhi13BpWRvn3s"
-app_name = "TestChatBox"
-m_key = "tejash_NLP"
+access_token = raw_input("Please enter your Access Token")
+app_name = raw_input("Please enter the name of your App")
+m_key = raw_input("Please enter the name of your m_key")
